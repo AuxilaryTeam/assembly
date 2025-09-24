4767dbcf5ab1780e92d6d73f861927f6e95f9cb5
hailemichael121 <yihunaashe@gmail.com>
2025-09-24 09:57:41 +0300

    deleted un wanted unused folders

--- frontend/src/components/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="w-full p-1 bg-white flex justify-center border border-[#eecf88]  items-center text-center">
-      <p>© Bank of Abyssinia 2025. All rights reserved.</p>
-    </div>
-  );
-};
-
-export default Footer;
--- frontend/src/components/utils/mockApi.ts
+++
@@ -1,2 +0,0 @@
-import type { IssueItem } from "./types";
-
